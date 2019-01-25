6c2412b8549528b0972be81d0cb0e053ea338eb9
Soskat <sosnowska.kk@gmail.com>
2019-01-25 23:24:10 +0100

    base for buidings generator

---
+++ ggj-2019/Assets/Scripts/BuildingsGenerator/BuildingSegmentsDatabase.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class FloorScheme
+{
+    public GameObject EmptyWall;
+    public GameObject Stairs;
+    public GameObject SideWall;
+    [SerializeField] private GameObject sideWallR;
+    public GameObject SideWallR { get { return sideWallR ?? SideWall; } }
+    [SerializeField] private GameObject sideWallL;
+    public GameObject SideWallL { get { return sideWallL ?? SideWall; } }
+    public GameObject SideDoor;
+    public GameObject SideWindow;
+}
+
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Building Segments Database")]
+public class BuildingSegmentsDatabase : ScriptableObject
+{
+    public FloorScheme defaultFloor;
+    public FloorScheme defaultRoof;
+
+    public List<FloorScheme> floorsSegments = new List<FloorScheme>();
+    public List<FloorScheme> roofsSegments = new List<FloorScheme>();
+
+
+    public FloorScheme GetRandomFloorScheme()
+    {
+        if (floorsSegments == null || floorsSegments.Count == 0)
+        {
+            return defaultFloor;
+        }
+        else
+        {
+            int index = Random.Range(0, floorsSegments.Count);
+            return floorsSegments[index];
+        }
+    }
+    public FloorScheme GetRandomRoofScheme()
+    {
+        if (roofsSegments == null || roofsSegments.Count == 0)
+        {
+            return defaultRoof;
+        }
+        else
+        {
+            int index = Random.Range(0, roofsSegments.Count);
+            return roofsSegments[index];
+        }
+    }
+}
---
+++ ggj-2019/Assets/Scripts/BuildingsGenerator/BuildingsGenerator.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BuildingsGenerator
+{
+    private BuildingSegmentsDatabase buildingsDatabase;
+
+
+    public BuildingsGenerator()
+    {
+        buildingsDatabase = Resources.Load<BuildingSegmentsDatabase>("Databases/BuildingSegmentsDatabase");
+    }
+
+
+
+    public void GenerateBuilding(Transform root, int floorSegmentsCount, int buildingFloorsCount, int stairsSegmentIndex)
+    {
+        Debug.Log($"Generated building with {floorSegmentsCount} segments width, {buildingFloorsCount} floors and stairs at each {stairsSegmentIndex} floor segment");
+    }
+}
