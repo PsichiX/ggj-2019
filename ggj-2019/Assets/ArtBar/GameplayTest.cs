659dd09cd989bb939625144e3f192a7d0466fb14
ArtBarSzu <szuler@o2.pl>
2019-01-26 12:49:53 +0100

    gameplay VIP

---
+++ ggj-2019/Assets/ArtBar/GameplayTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayTest : MonoBehaviour
+{
+    private GameplayManager gamplayManager;
+    void Start()
+    {
+        gamplayManager = GameplayManager.GetGameplayManager();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            gamplayManager.AttachToEvent(GamePhases.GameplayPhase.FloorEvacuationStart, EvacuationStart);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            int level = 77;
+            gamplayManager.CallEvent(GamePhases.GameplayPhase.FloorEvacuationStart, level);
+        }
+    }
+
+    private void EvacuationStart(System.Object param)
+    {
+        if(param is int)
+        {
+            int level = (int)param;
+
+            Debug.Log($"ddddd level {level}");
+        }
+
+    }
+
+
+
+}
