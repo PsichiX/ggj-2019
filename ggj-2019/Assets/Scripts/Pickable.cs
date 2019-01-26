711d30894c298a570b6a8d6ce13991dccacd23d6
Patryk 'PsichiX' Budzynski <psichix@gmail.com>
2019-01-26 19:11:08 +0100

    pickables changing layer when used

--- ggj-2019/Assets/Scripts/Pickable.cs
+++ ggj-2019/Assets/Scripts/Pickable.cs
@@ -18,17 +18,20 @@ namespace GaryMoveOut
         public void PickUp()
         {
             m_rigidBody.bodyType = RigidbodyType2D.Kinematic;
+            gameObject.layer = LayerMask.NameToLayer("FurnitureInUse");
         }
 
         public void PutDown()
         {
             m_rigidBody.bodyType = RigidbodyType2D.Dynamic;
+            gameObject.layer = LayerMask.NameToLayer("Furniture");
         }
 
         public void Throw(Vector2 impulse)
         {
             m_rigidBody.bodyType = RigidbodyType2D.Dynamic;
             m_rigidBody.AddForce(impulse, ForceMode2D.Impulse);
+            gameObject.layer = LayerMask.NameToLayer("Furniture");
         }
     }
 }
