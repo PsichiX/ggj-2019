aab63c33f990b0be99182505290f14d7392a50ea
Gradir <gradir@gmail.com>
2019-01-27 12:37:37 +0100

    glass shattering

--- ggj-2019/Assets/Scripts/Items/ItemCatcher.cs
+++ ggj-2019/Assets/Scripts/Items/ItemCatcher.cs
@@ -6,7 +6,9 @@ namespace GaryMoveOut
     public class ItemCatcher : MonoBehaviour
     {
         [SerializeField] private bool isInside = true;
-        private Collider coll;
+		[SerializeField] private AddForce addForce;
+		[SerializeField] private GameObject glassSheet;
+		private Collider coll;
 
         private Floor floor;
 
@@ -15,20 +17,44 @@ namespace GaryMoveOut
             this.floor = floor;
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            var item = other.GetComponent<Item>();
+            var item = other.GetComponent<ItemScheme>();
+			var item2 = new Item(item);
             if (item != null && floor != null)
             {
                 if (isInside)
                 {
-                    floor.AddItem(item);
+                    floor.AddItem(item2);
                 }
                 else
                 {
-                    floor.RemoveItem(item);
+                    floor.RemoveItem(item2);
                 }
             }
+			if (addForce != null)
+			{
+				var rigid = other.GetComponent<Rigidbody2D>();
+				if (rigid)
+				{
+					if (rigid.velocity.x > 0)
+					{
+						Pekaj(true);
+					}
+					else
+					{
+						Pekaj(false);
+					}
+				}
+			}
         }
+
+		private void Pekaj(bool wPrawo)
+		{
+			glassSheet.SetActive(false);
+			addForce.gameObject.SetActive(true);
+			addForce.right = wPrawo;
+			addForce.Peknij();
+		}
     }
 }
