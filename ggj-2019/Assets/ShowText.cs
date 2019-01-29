4513853076abd12d5fedc69f461562b3806f952a
Gradir <gradir@gmail.com>
2019-01-29 23:55:37 +0100

    misc

---
+++ ggj-2019/Assets/ShowText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using GaryMoveOut;
+using DG.Tweening;
+
+public class ShowText : MonoBehaviour
+{
+	[SerializeField] private TextMeshPro textMesh;
+
+	private void Start()
+	{
+		GameplayEvents.GetGameplayEvents().AttachToEvent(GamePhases.GameplayPhase.TruckStart, ShowIt);
+	}
+
+	private void ShowIt(object obj)
+	{
+		textMesh.DOFade(0.8f, 7.5f).SetEase(Ease.InExpo);
+		DOVirtual.DelayedCall(8f, () => textMesh.DOFade(0, 3f));
+	}
+}
